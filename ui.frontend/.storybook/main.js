fefcba1a7aaac40ad7508abea929af618937b819
sam <sampath.vnk243@protonmail.com>
2026-01-29 21:54:06 +1100

    storybook changes

--- ui.frontend/.storybook/main.js
+++ ui.frontend/.storybook/main.js
@@ -1,24 +1,19 @@
 module.exports = {
-  stories: ['../src/**/*.stories.@(js|ts)'],
-  addons: ['@storybook/addon-essentials'],
-  framework: '@storybook/html-webpack5',
+  stories: ["../src/**/*.stories.@(js|ts)"],
+  addons: ["@storybook/addon-essentials"],
+  framework: "@storybook/html-webpack5",
 
   webpackFinal: async (config) => {
     // Handlebars loader
     config.module.rules.push({
       test: /\.hbs$/,
-      loader: 'handlebars-loader',
+      loader: "handlebars-loader",
     });
 
     // SCSS loader
     config.module.rules.push({
       test: /\.scss$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader',
-      ],
-      include: /src/,
+      use: ["style-loader", "css-loader", "sass-loader"],
     });
 
     return config;
--- ui.frontend/src/main/webpack/components/button/button.js
+++ ui.frontend/src/main/webpack/components/button/button.js
@@ -1,25 +1,4 @@
-document.addEventListener('click', function (e) {
-    const btn = e.target.closest('.cmp-button');
-    if (!btn || btn.disabled) return;
-  
-    const action = btn.dataset.action;
-    const url = btn.dataset.url;
-  
-    switch (action) {
-      case 'alert':
-        alert(`Clicked: ${btn.textContent}`);
-        break;
-      case 'log':
-        console.log('Button clicked:', btn.textContent);
-        break;
-      case 'navigate':
-        if (url) window.location.href = url;
-        break;
-      case 'toggle':
-        btn.classList.toggle('cmp-button--active');
-        break;
-      default:
-        console.log('No action defined');
-    }
-  });
-  
+import template from "./button.hbs";
+import "./button.scss";
+
+export const Button = (args) => template(args);
